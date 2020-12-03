51f72f937bd5e3e39746b9cfcba1745a928a450f
MugishaU <muwiragiye123@gmail.com>
2020-12-03 18:14:06 +0000

    add different types of spirals Chp96

--- src/main/scala/Chapter9/Chp96.scala
+++ src/main/scala/Chapter9/Chp96.scala
@@ -10,7 +10,13 @@ import doodle.java2d._
 object Chp96 {
   def parametricCircle(angle: Angle): Point = Point.polar(200, angle)
   def parametricSpiral(angle: Angle): Point = Point((Math.exp(angle.toTurns) - 1)*150, angle)
+  val rose7 = (angle: Angle) => Point((angle * 8).cos * 200, angle)
+  def lissajous(a: Int, b: Int, offset: Angle): Angle => Point = (angle: Angle) =>
+    Point(100 * ((angle * a) + offset).sin, 100 * (angle * b).sin)
+  def epicycloid(a: Int, b: Int, c: Int): Angle => Point = (angle: Angle) =>
+    (Point(75, angle * a).toVec + Point(32, angle * b).toVec + Point(15, angle * c).toVec).toPoint
 
+  val dot:Image = Image.circle(3).fillColor(Color.limeGreen).strokeColor(Color.lawngreen)
   def sample(samples: Int, dot: Image,curve: Angle => Point): Image = {
     val step = Angle.one / samples
 
@@ -26,6 +32,6 @@ object Chp96 {
   }
 
   def main(args: Array[String]): Unit = {
-    sample(100,Image.circle(10).fillColor(Color.limeGreen).strokeColor(Color.lawngreen) ,parametricSpiral).draw()
+    sample(500,dot,epicycloid(7,13,25)).draw()
   }
 }
